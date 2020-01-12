58600abf61127ae1ca719a760b997db266318c76
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2020-01-11 20:30:23 -0500

    Overlay: Seperate overlays from vendor/aosip[2/2]
    Change-Id: If5a33b5b126af6028764d1e623ed6af64faa7dbe

--- overlays.mk
+++ overlays.mk
@@ -2,4 +2,20 @@
 DEVICE_PACKAGE_OVERLAYS += vendor/overlay/common/
 
 # Extra packages
-# PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \
+    AOSiPOverlayStub \
+
+# Accents
+include vendor/overlay/accents.mk
+
+# Bootanimation
+include vendor/overlay/bootanimation.mk
+
+# Fonts
+include vendor/overlay/fonts.mk
+
+# Sounds
+include vendor/overlay/sounds.mk
+
+# Prebuilts
+$(call inherit-product-if-exists, vendor/prebuilts/packages.mk)
