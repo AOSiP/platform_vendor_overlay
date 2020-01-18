e0e50dd8bdf9e191094bf37d9bde10e410bd5183
Alex <du.alexcruz@gmail.com>
2020-01-17 22:00:55 -0500

    Fonts: Import fonts.xml from fwb
    Commit history can be found here
    https://github.com/DirtyUnicorns/android_frameworks_base/commits/q10x/data/fonts/fonts.xml
    
    aosip edit:
    remove current font stuff
    
    Change-Id: Iae43a32ae2d3aa480af07508fd5e71801269a336

--- fonts.mk
+++ fonts.mk
@@ -1,10 +1,3 @@
 # Fonts
-LOCAL_PATH := vendor/overlay/fonts
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
-    $(LOCAL_PATH)/GoogleSans-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Medium.ttf \
-    $(LOCAL_PATH)/GoogleSans-MediumItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-MediumItalic.ttf \
-    $(LOCAL_PATH)/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
-    $(LOCAL_PATH)/GoogleSans-Bold.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Bold.ttf \
-    $(LOCAL_PATH)/GoogleSans-BoldItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-BoldItalic.ttf \
-    $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+PRODUCT_PACKAGES := \
+    fonts.xml
