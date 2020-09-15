5bcc2f5576fb42cbba54123013a281c7eef4914e
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2020-09-14 22:42:02 -0400

    TEMP: fix some build breakages
    also comment out some things that are unneeded at the moment
    
    Change-Id: I8eb29b8643147897b2cb35d8923bb3d02feaf24c

--- overlays.mk
+++ overlays.mk
@@ -4,19 +4,22 @@ DEVICE_PACKAGE_OVERLAYS += vendor/overlay/common
 # Allow overlays to be excluded from enforcing RRO
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlay/common
 
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/media/bootanimation.zip
+
 # Extra packages
 PRODUCT_PACKAGES += \
-    AOSiPOverlayStub \
-    PrimaryBlackOverlay \
-    PrimaryCharcoalBlackOverlay \
-    PrimaryClearOverlay \
-    PrimaryEtherealOverlay \
-    PrimaryFlameOverlay \
-    PrimaryNatureOverlay \
-    PrimaryOceanOverlay \
-    PrimarySolarizedOverlay \
-    EmptyOverlay \
     MatchmakerOverlay
+#    AOSiPOverlayStub \
+#    PrimaryBlackOverlay \
+#    PrimaryCharcoalBlackOverlay \
+#    PrimaryClearOverlay \
+#    PrimaryEtherealOverlay \
+#    PrimaryFlameOverlay \
+#    PrimaryNatureOverlay \
+#    PrimaryOceanOverlay \
+#    PrimarySolarizedOverlay \
+#    EmptyOverlay \
 
 # Accents
 include vendor/overlay/accents.mk
@@ -25,18 +28,18 @@ include vendor/overlay/accents.mk
 include vendor/overlay/bootanimation.mk
 
 # Fonts
-include vendor/overlay/fonts.mk
+#include vendor/overlay/fonts.mk
 
 # Gapps
-ifeq ($(WITH_GAPPS),true)
-$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
-endif
+#ifeq ($(WITH_GAPPS),true)
+#$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
+#endif
 
 # LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/overlay/dictionaries
 
 # Plugins
-include packages/apps/PotatoPlugins/plugins.mk
+#include packages/apps/PotatoPlugins/plugins.mk
 
 # Sounds
 include vendor/overlay/sounds.mk
