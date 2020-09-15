de4b13e82d3b7135b34c9cd4cf54a2f87e8272ca
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2020-09-14 22:42:02 -0400

    overlay: Adjustments for eleven
    pixelgapps>kronicgapps
    also comment out some things that are unneeded at the moment
    
    Change-Id: I8eb29b8643147897b2cb35d8923bb3d02feaf24c

--- overlays.mk
+++ overlays.mk
@@ -6,17 +6,17 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/overlay/common
 
 # Extra packages
 PRODUCT_PACKAGES += \
+    MatchmakerOverlay \
     AOSiPOverlayStub \
     PrimaryBlackOverlay \
-    PrimaryCharcoalBlackOverlay \
-    PrimaryClearOverlay \
-    PrimaryEtherealOverlay \
-    PrimaryFlameOverlay \
-    PrimaryNatureOverlay \
-    PrimaryOceanOverlay \
-    PrimarySolarizedOverlay \
-    EmptyOverlay \
-    MatchmakerOverlay
+#    PrimaryCharcoalBlackOverlay \
+#    PrimaryClearOverlay \
+#    PrimaryEtherealOverlay \
+#    PrimaryFlameOverlay \
+#    PrimaryNatureOverlay \
+#    PrimaryOceanOverlay \
+#    PrimarySolarizedOverlay \
+#    EmptyOverlay \
 
 # Accents
 include vendor/overlay/accents.mk
@@ -29,14 +29,14 @@ include vendor/overlay/fonts.mk
 
 # Gapps
 ifeq ($(WITH_GAPPS),true)
-$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
+$(call inherit-product-if-exists, vendor/gapps/gapps.mk)
 endif
 
 # LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/overlay/dictionaries
 
 # Plugins
-include packages/apps/PotatoPlugins/plugins.mk
+#include packages/apps/PotatoPlugins/plugins.mk
 
 # Sounds
 include vendor/overlay/sounds.mk
