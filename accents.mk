19aa70914f635718fe65ec9641c42a2d853f02c7
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2021-01-10 16:02:03 -0500

    overlay: drop "branded" accents
    Change-Id: I98c5d91a2b4129108ce185c5ae3a0eb55496e4ca

--- accents.mk
+++ accents.mk
@@ -33,26 +33,3 @@ PRODUCT_PACKAGES += \
     Red \
     Teal \
     Yellow
-
-# Brand Accents
-PRODUCT_PACKAGES += \
-    AospaGreen \
-    AndroidOneGreen \
-    CocaColaRed \
-    DiscordPurple \
-    FacebookBlue \
-    InstagramCerise \
-    JollibeeCrimson \
-    MonsterEnergyGreen \
-    NextbitMint \
-    OneplusRed \
-    PepsiBlue \
-    PocophoneYellow \
-    RazerGreen \
-    SamsungBlue \
-    SpotifyGreen \
-    StarbucksGreen \
-    TwitchPurple \
-    TwitterBlue \
-    XboxGreen \
-    XiaomiOrange
