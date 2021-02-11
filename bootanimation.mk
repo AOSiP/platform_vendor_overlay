6719c1b845e8da51a5ded8a4cb9dcfdf313ec49b
calebcabob <calphonic@gmail.com>
2021-02-10 20:18:57 -0500

    Boot Animation: Add new boot animation from Roger Truttman*
    Also: To make sure we get the AOSiP boot animation
    on devices that don't specify TARGET_SCREEN_WIDTH
    size, we'll fall back on the 1080p version.
    
    @ROGERdotT gets a HUGE thanks for this gem!
    
    Change-Id: Ibeca046e8fa2efd038c8ccfacb94a946b0922ef4

--- bootanimation.mk
+++ bootanimation.mk
@@ -1,2 +1,9 @@
-PRODUCT_COPY_FILES += \
-    vendor/overlay/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+# Boot Animation: Defaults to 1080p version unless specified to use 1440p
+
+ifneq ($(TARGET_SCREEN_WIDTH), 1440)
+    PRODUCT_COPY_FILES += \
+        vendor/overlay/bootanimation/1440.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+else
+    PRODUCT_COPY_FILES += \
+        vendor/overlay/bootanimation/1080.zip:$(TARGET_COPY_OUT_SYSTEM)/media/bootanimation.zip
+endif
